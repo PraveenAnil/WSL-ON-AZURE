4999ea2d48d419106b9fd26ea0ee4cc791d8af24
PraveenAnil <praveendosth@gmail.com>
2018-07-08 23:23:09 +0530

    Create InstallWSL.ps1

---
+++ CustomScripts/InstallWSL.ps1
@@ -0,0 +1,17 @@
+<# 
+.File Name
+ - InstallHyperV.ps1
+ 
+.What calls this script?
+ - This is a PowerShell DSC run as a Custom Script extention called by an arm template
+
+.What does this script do?  
+ - Enable Windows Feature - Windows Subsystem for Linux and restarts the machine
+
+#>
+
+Set-ExecutionPolicy Unrestricted -Force
+
+#Enable Windows Feature and Reboot
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -Restart
+
